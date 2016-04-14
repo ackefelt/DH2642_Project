809732582ca2c861b0da939ed9022ca6e4f0c034
Aggressive <elias.altai@gmail.com>
2016-04-15 00:03:17 +0200

    fixees to the model

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var agendaPlannerApp = angular.module('agendaPlanner', ['ngRoute']);
+var agendaPlannerApp = angular.module('agendaPlanner', ['ngRoute','ngResource']);
 
 agendaPlannerApp.config(['$routeProvider',
   function($routeProvider) {
@@ -7,7 +7,8 @@ agendaPlannerApp.config(['$routeProvider',
         templateUrl: 'partials/home.html'
       }).
       when('/overview', {
-        templateUrl: 'partials/agendaOverview.html'
+        templateUrl: 'partials/agendaOverview.html',
+        controller: 'overviewCtrl'
       }).          
       when('/add', {
         templateUrl: 'partials/addActivity.html',
@@ -17,3 +18,37 @@ agendaPlannerApp.config(['$routeProvider',
         redirectTo: '/home'
       });
   }]);
+
+agendaPlannerApp.directive('overview', function() {
+    return {
+        restrict: "E",
+        template: "<div class='col-md-3 addDay'>" +
+                  "<a href='#/overview' data-ng-click='append()' class='btn addDayBtn'>+ Add a day</a>"+
+                  "</div>",
+        controller: function($scope, $element, $attrs) {
+            $scope.append = function() {
+              var newDay =   " <div class='col-md-3 day'>" +
+        "<div class='row dayHeader'>"+
+            "<div class='col-md-9 dayInfo'>"+
+            "<p>Start time: {{this.getDays()[0].getStart()}} </p>"+
+            "<p>End time: {{this.getDays()[0].getEnd()}} </p>"+
+            "<p>Total length: {{this.getDays()[0].getTotalLength()}} min</p>"+
+            "</div>"+
+            "<div class='col-md-3 timeAllocatedBox'>"+
+            "</div>"+
+        "</div>"+
+    "<div>"+
+        "<div class='row schedule'>"+
+            "<a ng-repeat='activity in days[0].getActivities()' {{this.days}} >"+
+            "<div class='row breakLine'>"+
+            "<p class='schedule-text'> {{this.activity.getLength()}} min {{this.activity.getName()}} </p>"+
+            "</div>" +
+             "</a>" +
+        "</div>" +
+    "</div>" +
+"</div>";
+                $element.append(newDay);
+            }
+        }
+    }
+});
