8aa9227319f6f1bf0c27c978dc756e59940d151a
Alfred Weidenbladh <alfred.weidenbladh@gmail.com>
2016-04-15 11:40:28 +0200

    add day

--- app/js/controllers/overviewCtrl.js
+++ app/js/controllers/overviewCtrl.js
@@ -8,4 +8,27 @@ agendaPlannerApp.controller('overviewCtrl', function ($scope,Agenda) {
  	$scope.getParkedActivities = function() {
  		return Agenda.getParkedActivities();
  	}
+
+ 	$scope.addDay = function() {
+ 		var newDays = angular.element(document.querySelector('#newDays'));
+ 		var txt = "<div class='col-md-3 day'>"+
+            "<div class='row dayHeader'>"+
+                "<div class='col-md-9 dayInfo'>"+
+                    "<p>Start time: {{this.getDays()[0].getStart()}} </p>"+
+                    "<p>End time: {{this.getDays()[0].getEnd()}} </p>"+
+                    "<p>Total length: {{this.getDays()[0].getTotalLength()}} min</p>"+
+                "</div>"+
+                "<div class='col-md-3 timeAllocatedBox'></div>"+
+            "</div>"+
+        "<div>"+
+            "<div class='row schedule'>"+
+                "<a ng-repeat='activity in days[0].getActivities()' {{this.days}} >"+
+                "<div class='row breakLine'>"+
+                    "<p class='schedule-text'> {{this.activity.getLength()}} min {{this.activity.getName()}} </p>"+
+                "</div>"+
+                "</a>"+
+            "</div>"+
+        "</div>";
+        newDays.append(txt);
+ 	}
 });
